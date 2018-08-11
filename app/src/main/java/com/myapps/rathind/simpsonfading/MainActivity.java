03d5110fe3bb887552cfb5bef9b82da03d23028f
Rathi <rathind14@gmail.com>
2018-08-11 12:29:50 -0400

    animated to vanish the bart's image in 2second

--- app/src/main/java/com/myapps/rathind/simpsonfading/MainActivity.java
+++ app/src/main/java/com/myapps/rathind/simpsonfading/MainActivity.java
@@ -2,8 +2,17 @@ package com.myapps.rathind.simpsonfading;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void fade(View view) {
+        Log.i("info","ImageView Clicked");
+        ImageView imageView  = (ImageView) findViewById(R.id.imageView);
+        imageView.animate().alpha(0).setDuration(2000);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
