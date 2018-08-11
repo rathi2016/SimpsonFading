c5900877ec9950116cea3757713c90a37384885c
Rathi <rathind14@gmail.com>
2018-08-11 12:53:03 -0400

    able to fade in and out

--- app/src/main/java/com/myapps/rathind/simpsonfading/MainActivity.java
+++ app/src/main/java/com/myapps/rathind/simpsonfading/MainActivity.java
@@ -8,11 +8,23 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean bartIsShowing = true;
+
     public void fade(View view) {
-        Log.i("info","ImageView Clicked");
-        ImageView imageView  = (ImageView) findViewById(R.id.imageView);
-        imageView.animate().alpha(0).setDuration(2000);
+        Log.i("info", "ImageView Clicked");
+        ImageView bartImageView = (ImageView) findViewById(R.id.imageView1);
+        ImageView homerImageView = (ImageView) findViewById(R.id.imageView2);
+        if (bartIsShowing) {
+            bartIsShowing = false;
+            bartImageView.animate().alpha(0).setDuration(2000);
+            homerImageView.animate().alpha(1).setDuration(2000);
+
+        } else {
+            bartImageView.animate().alpha(1).setDuration(2000);
+            homerImageView.animate().alpha(0).setDuration(2000);
+        }
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
